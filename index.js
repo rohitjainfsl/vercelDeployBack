f8e3a4496f25fdc7c417c0bac7f8bb8a6fb23856
Rohit Jain <rohit@fullstacklearning.com>
2023-11-09 09:34:58 +0530

    backend

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    "mongodb+srv://mongodbuser:zFMYcFgVV4SS7jA6@cluster0.4ont6qs.mongodb.net/vercelDeploy?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = new User({ email, password });
+    await user.save();
+    res.send(true);
+  } catch (err) {
+    console.log(err);
+    res.send(false);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+    if (user) {
+      res.send(true);
+    } else {
+      res.send(false);
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(false);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
