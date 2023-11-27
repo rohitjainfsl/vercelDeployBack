fc0c09f18a7c9f03ba1edf13e518f1ea11b6e3a2
Rohit Jain <rohit@fullstacklearning.com>
2023-11-27 19:54:02 +0530

    updated cors url

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin: 'https://vercel-deploy-front.onrender.com'}));
 
 mongoose
   .connect(
